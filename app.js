2b0a6ee571ffaa9a8fb9c89ded10e3647862129b
AgomohC <chinaemerem98@yahoo.com>
2021-09-10 20:27:19 +0100

    finished app

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+// require express
 const express = require("express");
+
+//initialize express
 const app = express();
+
+//middleware
 app.use(express.static("./public"));
+
+//routes
+app.get("/api/whoami", (req, res) => {
+  const ip = req.ip;
+  const userAgent = req.headers["user-agent"];
+  const lang = req.headers["accept-language"];
+
+  res.json({ ipaddress: ip, language: lang, software: userAgent });
+});
+
+//port
 const port = process.env.PORT || 8080;
+
+//listener function
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
